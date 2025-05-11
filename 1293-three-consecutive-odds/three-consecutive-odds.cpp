0ecb2ffc0e22107fca1dab5a6f811821c7f7da17
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-05-11 19:11:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (59.60%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n = arr.size();
+        int count =0;
+        for(int i =0;i<n;i++)
+        {
+            if(arr[i]%2 == 1)
+            {
+             count++;
+             if(count == 3)
+             {
+                 return true;
+             }
+            }
+            else
+            {
+              count = 0;
+            }
+        }
+      return false; 
+
+    }
+    
+    
+};
