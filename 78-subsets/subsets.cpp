dc075c1ab8564c66298aa6ce07c5a0efa32ce95e
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-05-16 12:30:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (61.55%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void backtrack(vector<vector<int>>& ans, vector<int>& subset, vector<int>& nums, int start) {
-        ans.push_back(subset); // Push the current subset to the answer
+        ans.push_back(subset); 
         for (int i = start; i < nums.size(); ++i) {
-            subset.push_back(nums[i]); // Include the current element
-            backtrack(ans, subset, nums, i + 1); // Recur for the next elements
-            subset.pop_back(); // Backtrack by removing the current element
+            subset.push_back(nums[i]); 
+            backtrack(ans, subset, nums, i + 1); 
+            subset.pop_back(); 
         }
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        sort(nums.begin(), nums.end()); // Sorting is optional but can help with ordered output
+        sort(nums.begin(), nums.end()); 
         vector<int> subset;
         backtrack(ans, subset, nums, 0);
         return ans;
