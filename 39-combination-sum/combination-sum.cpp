6ac9d1c611d73b79fb792af1987a4df34b863c1e
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-05-18 23:51:12 +0530

    Time: 6 ms (34.10%) | Memory: 14 MB (73.75%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public:
+        vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+                vector<vector<int>> result;
+                        vector<int> path;
+                                sort(candidates.begin(), candidates.end());
+                                        backtrack(candidates, 0, target, path, result);
+                                                return result;
+                                                    }
+
+                                                    private:
+                                                        void backtrack(const vector<int>& candidates, int start, int target, vector<int>& path, vector<vector<int>>& result) {
+                                                                if (target == 0) {
+                                                                            result.push_back(path);
+                                                                                        return;
+                                                                                                }
+                                                                                                        if (target < 0) {
+                                                                                                                    return;
+                                                                                                                            }
+                                                                                                                                    for (int i = start; i < candidates.size(); ++i) {
+                                                                                                                                                path.push_back(candidates[i]);
+                                                                                                                                                            backtrack(candidates, i, target - candidates[i], path, result);
+                                                                                                                                                                        path.pop_back();
+                                                                                                                                                                                }
+                                                                                                                                                                                    }
+                                                                                                                                                                                    };
