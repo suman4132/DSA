a8c46d82e8ea145832c36bf947e0ea2578b62790
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-05-22 12:45:20 +0530

    Time: 31 ms (26.95%) | Memory: 11.1 MB (49.06%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,24 +1,58 @@
 class Solution {
 public:
-  int palindromecheck(string s , int i,int j)
-  {
-    int ans =0;
-    while(i >= 0 && j < s.length() && s[i] == s[j])
-    {
-        ans++;
-        i--;
-        j++;
-    }
-    return ans;
-  }
-    int countSubstrings(string s) {
-        int n = s.length();
-        int ans=0;
-        for(int i =0;i<n;i++)
-        {
-           ans+=palindromecheck(s ,i,i);
-           ans+=palindromecheck(s ,i,i+1);
+//   int palindromcheck(string s , int i,int j)
+//   {
+//     int ans =0;
+//     while(i >= 0 && j < s.length() && s[i] == s[j])
+//     {
+//         ans++;
+//         i--;
+//         j++;
+//     }
+//     return ans;
+//   }
+
+
+/*dp approach*/
+int countOfPalindrom(string s)
+{
+    int n = s.length();
+        int count = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                if (s[i] == s[j]) {
+                    if (len == 1 || len == 2)
+                        dp[i][j] = true;
+                    else
+                        dp[i][j] = dp[i + 1][j - 1];
+                }
+
+                if (dp[i][j])
+                    count++;
+            }
         }
-         return ans;
+
+        return count;
+    }
+
+
+
+
+
+    int countSubstrings(string s) {
+        // int n = s.length();
+        // int ans=0;
+        // for(int i =0;i<n;i++)
+        // {
+        //    ans+=palindromcheck(s ,i,i);
+        //    ans+=palindromcheck(s ,i,i+1);
+        // }
+        //  return ans;
+
+        return countOfPalindrom(s);
     }
 };
