b57d85fa7be11399f67bf6dd5f6c4e12521393e1
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-06-05 13:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (49.17%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int>s;
+        for(int i : nums)
+        {
+            if(i > 0)
+            {
+                s.insert(i);
+            }
+        }
+        return s.size();
+    }
+};
