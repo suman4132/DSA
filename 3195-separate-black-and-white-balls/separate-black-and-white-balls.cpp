913fed9e3ed3ff99d4304f69383cdf546ca1cd36
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-06-09 12:15:17 +0530

    Time: 4 ms (72.10%) | Memory: 17.2 MB (68.87%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long  white = 0;  
+        long long  res = 0;    
+        int n = s.length();
+        
+        for (int j = 0; j < n; ++j) {
+            if (s[j] == '0') {
+                res += white; 
+            } else if (s[j] == '1') {
+                white++;  
+            }
+        }
+        
+        return res;
+    }
+};
