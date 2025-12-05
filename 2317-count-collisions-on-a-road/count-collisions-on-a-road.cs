a2651852b44ec357c334bb37459683de2904c433
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-12-05 23:34:19 +0530

    Time: 9 ms (60.00%) | Memory: 45.9 MB (100.00%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int CountCollisions(string directions) {
+        int i =0, j = directions.Length -1;
+        while(i < directions.Length && directions[i] == 'L') i++;
+        while(j >= 0 && directions[j] == 'R') j--;
+        int count = 0;
+
+        for(int k = i ;k <= j; k++)
+          if(directions[k] != 'S') count++;
+        return count;  
+    }
+}
