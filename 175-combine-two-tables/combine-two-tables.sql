3aa452ef4cb7c5743e479143e7705e25d9f18d03
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2025-12-06 00:22:26 +0530

    Time: 433 ms (17.95%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,7 @@
-select  Person.firstName , Person.lastName , Address.city, Address.state
-from Person
-left join Address
+select Person.firstName,Person.lastName, Address.city, Address.state
+
+from Person 
+
+left join Address 
+
 on Person.personId = Address.personId;
