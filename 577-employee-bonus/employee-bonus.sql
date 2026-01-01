352874e7326942886c8811bffeb97b7eeb57c067
Suman Kumar Sharma <161910949+suman4132@users.noreply.github.com>
2026-01-01 19:48:02 +0530

    Time: 1064 ms (17.32%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,5 @@
 select name, bonus 
 from Employee
 left join Bonus 
-on Employee.empId = Bonus.empId where bonus < 1000 or bonus is null;
+on Employee.empId = Bonus.empId 
+where bonus < 1000 or bonus is null;
